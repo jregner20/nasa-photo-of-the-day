ba7633343520449bbbe661ff31afc51009dcf692
Jake Regner <jregner20@gmail.com>
2019-11-06 15:39:50 -0500

    Add axios and set up axios call to import photo of the day, display in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import NasaGrid from "./NasaGrid";
 
 function App() {
   return (
     <div className="App">
+      <NasaGrid/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
