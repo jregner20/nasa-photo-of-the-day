216c40d9b827c7c8ce4c44f70b0bf1bc20864e8a
Jake Regner <jregner20@gmail.com>
2019-11-07 16:01:43 -0500

    Add starting styling and positioning of explanation card component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import NasaGrid from "./NasaGrid";
 import NasaDate from "./Header";
 import Explanation from "./Explanation";
+import ExplanationCard from "./ExplanationCard";
 
 function App() {
   return (
     <div className="App">
       <NasaDate/>
       <NasaGrid/>
-      <Explanation/>
+      <ExplanationCard>
+        <Explanation/>
+      </ExplanationCard>
     </div>
   );
 }
